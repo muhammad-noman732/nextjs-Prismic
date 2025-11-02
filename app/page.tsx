2e68f03508fb328107ca13be89f100b3dee89230
muhammad-noman732 <iasknoman156@gmail.com>
2025-11-02 14:44:48 -0800

    Feat: Add Homepage page Type

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
+import { type Metadata } from "next";
+import { notFound } from "next/navigation";
+import { asImageSrc } from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
 
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className="font-sono text-5xl font-bold">Whereas recognition of the inherent dignity</h1>
-    </div>
-  );
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage").catch(() => notFound());
+
+   return <div>It worked</div>
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage").catch(() => notFound());
+
+  
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+    openGraph: {
+      images: [{ url: asImageSrc(page.data.meta_image) ?? "" }],
+    },
+  };
 }
